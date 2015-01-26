f2e006336fb667206dc708228317adbc97ca0965
Hung Tran <oohnoitz@gmail.com>
2015-01-25 22:09:04 -0600

    updated plugin hooks

--- bootstrap.php
+++ bootstrap.php
@@ -24,7 +24,7 @@ class HHVM_URIMediaPurge
                     ->register('foolfuuka-plugin.uri_media_purge', 'Foolz\Foolfuuka\Plugins\URIMediaPurge\Model\URIMediaPurge')
                     ->addArgument($context);
 
-                Event::forge('Foolz\Foolframe\Model\Context::handleWeb#obj.afterAuth')
+                Event::forge('Foolz\FoolFrame\Model\Context::handleWeb#obj.afterAuth')
                     ->setCall(function ($result) use ($context) {
                         // don't add the admin panels if the user is not an admin
                         if ($context->getService('auth')->hasAccess('maccess.admin')) {
@@ -41,7 +41,7 @@ class HHVM_URIMediaPurge
                                 )
                             );
 
-                            Event::forge('Foolz\Foolframe\Controller\Admin::before#var.sidebar')
+                            Event::forge('Foolz\FoolFrame\Controller\Admin::before#var.sidebar')
                                 ->setCall(function ($result) {
                                     $sidebar = $result->getParam('sidebar');
                                     $sidebar[]['plugins'] = [
